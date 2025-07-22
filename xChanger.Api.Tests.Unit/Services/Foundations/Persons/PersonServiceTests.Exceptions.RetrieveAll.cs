b5bd900939e1fc09526551ce81498362fe6430d9
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 00:55:04 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,54 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using xChanger.Api.Models.Foundations.Persons.Exceptions;
+
+namespace xChanger.Api.Tests.Unit.Services.Foundations.Persons
+{
+    public partial class PersonServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedPersonStorageException =
+                new FailedPersonStorageException(sqlException);
+
+            var expectedPersonDependencyException =
+                new PersonDependencyException(failedPersonStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllPersons()).Throws(sqlException);
+
+            // when
+            Action retrieveAllPeopleAction = () =>
+                this.personService.RetrieveAllPersons();
+
+            PersonDependencyException actualPersonDependencyException =
+                Assert.Throws<PersonDependencyException>(retrieveAllPeopleAction);
+
+            // then
+            actualPersonDependencyException.Should()
+                .BeEquivalentTo(expectedPersonDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllPersons(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPersonDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
