9d700d6b3260695e5810ba78dc028410abe97125
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-22 17:45:20 +0500

    INFRA: Initialize Unit Test Project

---
+++ xChanger.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+namespace xChanger.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
