ba3ed5b3b0cf472b44aa5fb71959c07bd61c60e4
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 01:28:43 +0500

    ShouldThrowServiceExceptionOnRetrieveByIdAsyncIfServiceErrorOccursAndLogItAsync -> FAIL

--- xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Exceptions.RetrieveById.cs
+++ xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Exceptions.RetrieveById.cs
@@ -54,5 +54,47 @@ namespace xChanger.Api.Tests.Unit.Services.Foundations.Persons
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdAsyncIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var serverException = new Exception();
+
+            var failedPersonServiceException =
+                new FailedPersonServiceException(serverException);
+
+            var expectedPersonServiceException =
+                new PersonServiceException(failedPersonServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPersonByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serverException);
+
+            // when
+            ValueTask<Person> retrievePersonById =
+                this.personService.RetrievePersonByIdAsync(someId);
+
+            PersonServiceException actualPersonServiceException =
+                await Assert.ThrowsAsync<PersonServiceException>(() =>
+                    retrievePersonById.AsTask());
+
+            // then
+            actualPersonServiceException.Should()
+                .BeEquivalentTo(expectedPersonServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPersonByIdAsync(someId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPersonServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
