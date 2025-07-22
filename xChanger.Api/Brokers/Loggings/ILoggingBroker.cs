6598dfa512b1d41716e022764e3ee4a10a98f863
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-22 23:42:07 +0500

    BROKERS: Implement Logging

---
+++ xChanger.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,13 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+namespace xChanger.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
