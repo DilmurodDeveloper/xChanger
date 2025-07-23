b479eeedc508f1e5478c61ab3bec12af76fd344c
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 16:52:33 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,64 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using xChanger.Api.Models.Foundations.Pets;
+using xChanger.Api.Models.Foundations.Pets.Exceptions;
+
+namespace xChanger.Api.Tests.Unit.Services.Foundations.Pets
+{
+    public partial class PetServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Pet randomPet = CreateRandomPet();
+            Pet somePet = randomPet;
+            Guid petId = somePet.Id;
+            SqlException sqlException = GetSqlError();
+
+            var failedPetStorageException =
+                new FailedPetStorageException(sqlException);
+
+            var expectedPetDependencyException =
+                new PetDependencyException(failedPetStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPetByIdAsync(petId))
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Pet> modifyPetTask =
+                this.petService.ModifyPetAsync(somePet);
+
+            PetDependencyException actualPetDependencyException =
+                await Assert.ThrowsAsync<PetDependencyException>(() =>
+                    modifyPetTask.AsTask());
+
+            // then
+            actualPetDependencyException.Should()
+                .BeEquivalentTo(expectedPetDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPetDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPetByIdAsync(petId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePetAsync(somePet),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
