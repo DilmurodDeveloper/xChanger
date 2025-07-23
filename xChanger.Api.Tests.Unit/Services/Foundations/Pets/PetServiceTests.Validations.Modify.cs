651524365837db505bda82baf9c37f62c2a14f4d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 16:45:47 +0500

    ShouldThrowValidationExceptionOnModifyIfPetIsInvalidAndLogItAsync -> FAIL

--- xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Validations.Modify.cs
+++ xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Validations.Modify.cs
@@ -46,5 +46,60 @@ namespace xChanger.Api.Tests.Unit.Services.Foundations.Pets
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPetIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            Pet invalidPet = new Pet
+            {
+                Name = invalidText
+            };
+
+            var invalidPetException = new InvalidPetException();
+
+            invalidPetException.AddData(
+                key: nameof(Pet.Id),
+                values: "Id is required");
+
+            invalidPetException.AddData(
+                key: nameof(Pet.Name),
+                values: "Text is required");
+
+            invalidPetException.AddData(
+                key: nameof(Pet.PersonId),
+                values: "Id is required");
+
+            var expectedPetValidationException =
+                new PetValidationException(invalidPetException);
+
+            // when
+            ValueTask<Pet> modifyPetTask =
+                this.petService.ModifyPetAsync(invalidPet);
+
+            PetValidationException actualPetValidationException =
+                await Assert.ThrowsAsync<PetValidationException>(
+                    modifyPetTask.AsTask);
+
+            // then
+            actualPetValidationException.Should()
+                .BeEquivalentTo(expectedPetValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPetValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePetAsync(It.IsAny<Pet>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
