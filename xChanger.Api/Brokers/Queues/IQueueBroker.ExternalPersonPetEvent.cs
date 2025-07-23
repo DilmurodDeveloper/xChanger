d7d8721cc528465462d3145c00f9aff468eb2c45
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 18:10:59 +0500

    BROKERS: Implement Queue

---
+++ xChanger.Api/Brokers/Queues/IQueueBroker.ExternalPersonPetEvent.cs
@@ -0,0 +1,15 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using xChanger.Api.Models.Foundations.ExternalPersons;
+
+namespace xChanger.Api.Brokers.Queues
+{
+    public partial interface IQueueBroker
+    {
+        ValueTask AddExternalPersonPets(List<ExternalPerson> externalPersonPets);
+        ValueTask<List<ExternalPerson>> ReadExternalPersonPets();
+    }
+}
