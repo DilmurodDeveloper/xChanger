9e2481d7214f4ed9bf85bc7313cf2d9ed8ede678
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 12:08:50 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Exceptions.Modify.cs
+++ xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Exceptions.Modify.cs
@@ -5,6 +5,7 @@
 
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using xChanger.Api.Models.Foundations.Persons;
 using xChanger.Api.Models.Foundations.Persons.Exceptions;
@@ -46,6 +47,54 @@ namespace xChanger.Api.Tests.Unit.Services.Foundations.Persons
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPersonDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPersonByIdAsync(personId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePersonAsync(somePerson),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            Person randomPerson = CreateRandomPerson();
+            Person somePerson = randomPerson;
+            Guid personId = somePerson.Id;
+            var databaseUpdateException = new DbUpdateException();
+
+            var failedPersonStorageException =
+                new FailedPersonStorageException(databaseUpdateException);
+
+            var expectedPersonDependencyException =
+                new PersonDependencyException(failedPersonStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectPersonByIdAsync(personId))
+                    .Throws(databaseUpdateException);
+
+            // when
+            ValueTask<Person> modifyPersonTask =
+                this.personService.ModifyPersonAsync(somePerson);
+
+            PersonDependencyException actualPersonDependencyException =
+                await Assert.ThrowsAsync<PersonDependencyException>(() =>
+                    modifyPersonTask.AsTask());
+
+            // then
+            actualPersonDependencyException.Should()
+                .BeEquivalentTo(expectedPersonDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
                     expectedPersonDependencyException))),
                         Times.Once);
 
