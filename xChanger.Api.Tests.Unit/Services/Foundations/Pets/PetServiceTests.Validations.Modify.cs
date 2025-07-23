d2e46f77fa6ef12f86509e39ce7bd9795e193fe5
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 16:42:59 +0500

    ShouldThrowValidationExceptionOnModifyIfPetIsNullAndLogItAsync -> FAIL

---
+++ xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Validations.Modify.cs
@@ -0,0 +1,50 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using FluentAssertions;
+using Moq;
+using xChanger.Api.Models.Foundations.Pets;
+using xChanger.Api.Models.Foundations.Pets.Exceptions;
+
+namespace xChanger.Api.Tests.Unit.Services.Foundations.Pets
+{
+    public partial class PetServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPetIsNullAndLogItAsync()
+        {
+            // given
+            Pet nullPet = null;
+            var nullPetException = new NullPetException();
+
+            var expectedPetValidationException =
+                new PetValidationException(nullPetException);
+
+            // when
+            ValueTask<Pet> modifyPetTask =
+                this.petService.ModifyPetAsync(nullPet);
+
+            PetValidationException actualPetValidationException =
+                await Assert.ThrowsAsync<PetValidationException>(
+                    modifyPetTask.AsTask);
+
+            // then
+            actualPetValidationException.Should()
+                .BeEquivalentTo(expectedPetValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPetValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePetAsync(It.IsAny<Pet>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
