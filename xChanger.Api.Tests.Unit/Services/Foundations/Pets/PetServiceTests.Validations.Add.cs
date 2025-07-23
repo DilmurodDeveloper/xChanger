24225b934ab2b923890a45e8823378a958367fab
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 13:06:52 +0500

    ShouldThrowValidationExceptionOnAddIfPetIsInvalidAndLogItAsync -> FAIL

--- xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Validations.Add.cs
+++ xChanger.Api.Tests.Unit/Services/Foundations/Pets/PetServiceTests.Validations.Add.cs
@@ -41,5 +41,56 @@ namespace xChanger.Api.Tests.Unit.Services.Foundations.Pets
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfPetIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidPet = new Pet
+            {
+                Name = invalidText
+            };
+
+            var invalidPetException = new InvalidPetException();
+
+            invalidPetException.AddData(
+                key: nameof(Pet.Id),
+                values: "Id is required");
+
+            invalidPetException.AddData(
+                key: nameof(Pet.Name),
+                values: "Text is required");
+
+            invalidPetException.AddData(
+                key: nameof(Pet.PersonId),
+                values: "Id is required");
+
+            var expectedPetValidationException =
+                new PetValidationException(invalidPetException);
+
+            // when
+            ValueTask<Pet> addPetTask =
+                this.petService.AddPetAsync(invalidPet);
+
+            // then
+            await Assert.ThrowsAsync<PetValidationException>(() =>
+                addPetTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPetValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPetAsync(It.IsAny<Pet>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ xChanger.Api/Models/Foundations/Pets/Exceptions/InvalidPetException.cs
@@ -0,0 +1,16 @@
+﻿//- - - - - - - - - - - - - - - - - - - - - - - - - -
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to Use for Precise File Conversion
+//- - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using Xeptions;
+
+namespace xChanger.Api.Models.Foundations.Pets.Exceptions
+{
+    public class InvalidPetException : Xeption
+    {
+        public InvalidPetException()
+            : base(message: "Pet is invalid.")
+        { }
+    }
+}
