b05ac0e6b06a33101a06c2c0650d4c91f160d538
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 16:07:51 +0500

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync -> PASS

--- xChanger.Api/Services/Foundations/Pets/PetService.cs
+++ xChanger.Api/Services/Foundations/Pets/PetService.cs
@@ -50,6 +50,18 @@ namespace xChanger.Api.Services.Foundations.Pets
 
                 throw petDependencyException;
             }
+            catch (Exception exception)
+            {
+                var failedPetServiceException =
+                    new FailedPetServiceException(exception);
+
+                var petServiceException =
+                    new PetServiceException(failedPetServiceException);
+
+                this.loggingBroker.LogError(petServiceException);
+
+                throw petServiceException;
+            }
         }
     }
 }
