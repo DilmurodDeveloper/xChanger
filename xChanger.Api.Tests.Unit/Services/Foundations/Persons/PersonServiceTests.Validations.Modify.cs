c9c4068e6da3265deb8afdb58382085ff7638f78
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-23 11:59:52 +0500

    ShouldThrowValidationExceptionOnModifyIfPersonIsInvalidAndLogItAsync -> FAIL

--- xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Validations.Modify.cs
+++ xChanger.Api.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Validations.Modify.cs
@@ -46,5 +46,60 @@ namespace xChanger.Api.Tests.Unit.Services.Foundations.Persons
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPersonIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidPerson = new Person
+            {
+                Name = invalidText
+            };
+
+            var invalidPersonException = new InvalidPersonException();
+
+            invalidPersonException.AddData(
+                key: nameof(Person.Id),
+                values: "Id is required");
+
+            invalidPersonException.AddData(
+                key: nameof(Person.Name),
+                values: "Text is required");
+
+            invalidPersonException.AddData(
+                key: nameof(Person.Age),
+                values: "Value must be greater than 0");
+
+            var expectedPersonValidationException =
+                new PersonValidationException(invalidPersonException);
+
+            // when
+            ValueTask<Person> modifyPersonTask =
+                this.personService.ModifyPersonAsync(invalidPerson);
+
+            PersonValidationException actualPersonValidationException =
+                await Assert.ThrowsAsync<PersonValidationException>(
+                    modifyPersonTask.AsTask);
+
+            // then
+            actualPersonValidationException.Should()
+                .BeEquivalentTo(expectedPersonValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPersonValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePersonAsync(It.IsAny<Person>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
